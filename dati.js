580349da9ad9d3767032cc2d29a73584784b6c60
Paolo224 <picariellopaolo224@gmail.com>
2022-11-25 15:46:00 +0100

    Bozza 2

--- dati.js
+++ dati.js
@@ -2,13 +2,12 @@ const number = [];
 
 let userNumber;
 
-
 for( let i = 0; i < 6; i++){
 
     userNumber = prompt('inserisci un numero');
 
     if(userNumber % 2 == 1){
-        userNumber += number;
+        userNumber = number;
     }
 }
 
