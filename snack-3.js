fc40a62c56953b2c29cbd08fac2fc908bc8e3b31
Paolo224 <picariellopaolo224@gmail.com>
2022-11-25 17:44:40 +0100

    Snack-3 completo( somma, media e massimo )

--- snack-3.js
+++ snack-3.js
@@ -3,29 +3,37 @@ const numbers = [
     20,
     30,
     27,
-    10,
-    20,
-    30,
-    27,
-    10,
-    20,
-    30,
-    27,
-    10,
-    20,
-    30,
-    27,
+    38,
+    82,
+    93,
+    53,
+    39,
+    80,
+    12,
+    29,
+    50,
+    25,
+    2,
+    9,
 ]
 
 let somma = 0;
 
 let media = 0;
 
+let valoreMassimo = numbers[0];
+
 for( let i = 0; i < 10; i++){
     somma += numbers[i];
+    
+    if(numbers[i] > valoreMassimo){
+        valoreMassimo = numbers[i];
+    }
+    
 }
 
 media = somma / 10;
 
 console.log(somma);
 console.log(media);
+console.log(valoreMassimo);
