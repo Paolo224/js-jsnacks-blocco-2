5d7c5aa9475fccb20845f9487eb9c48ab4db1cca
Paolo224 <picariellopaolo224@gmail.com>
2022-11-25 15:40:46 +0100

    Bozza primo snack

---
+++ dati.js
@@ -0,0 +1,15 @@
+const number = [];
+
+let userNumber;
+
+
+for( let i = 0; i < 6; i++){
+
+    userNumber = prompt('inserisci un numero');
+
+    if(userNumber % 2 == 1){
+        userNumber += number;
+    }
+}
+
+console.log(number);
